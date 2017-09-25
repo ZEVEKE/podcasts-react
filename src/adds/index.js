fb682f572f19e1fc7bbf495e85c2b54e8cf20b43
ZEVEKE <zeveke.developing@gmail.com>
2017-09-25 12:35:53 +0300

    feat($adds): create adds folder
    Add 'adds' folder, that contains necessary additional info-files that have JSON format:
    - categories.json
    - languages

---
+++ src/adds/index.js
@@ -0,0 +1,9 @@
+import categories from './categories.json';
+import languages from './languages.json';
+import podcasts from './podcasts.json';
+
+export default {
+    categories,
+    languages,
+    podcasts
+};
